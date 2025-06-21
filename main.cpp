d0d4d223b5af932c596f1402ab4edebac8288652
longjuncai <1789728370@qq.com>
2025-06-21 19:07:37 +0800

    🐛add override func

--- main.cpp
+++ main.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
+#include "template.h"
 int main(int argc, char** argv)
 {
-    std::cout<< "Hello world!"<<std::endl;
-    std::cout<< "Hello test a!"<<std::endl;
-    std::cout<< "Hello test b!"<<std::endl;
+    int result = sum(1, 2);
+    int a =1, b = 2;
+    sum(a, b);
+    const int c = 1, d = 2;
+    sum(c, d);
+    const int& num = a;
+    int* const p = &a;
+    Test<int, int> a1(p, p);
+    std::cout<< "sum result: "<<result << std::endl;
     return 0;
 }
 
-void display()
+void display(int a)
 {
-    std::cout<< "test.....";
+
 }
+
+void display(int& a)
+{
+
+}
+
+void display(const int& a)
+{
+
+}
+
+// void display(int* a)
+// {
+
+// }
+
+void display(const int* a)
+{
+
+}
+
+void display(int* const a)
+{
+
+}
+
